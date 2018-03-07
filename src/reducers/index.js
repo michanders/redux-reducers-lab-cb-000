5fcdeab0dc53545dc8d8c42fbf1fb1cfd565fc1c
Michael Anderson <michanders@msn.com>
2018-03-07 19:10:51 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,16 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
+    case 'REMOVE_INGREDIENT':
+      var s = []
+      state.map(i => {
+        if(i !== action.payload){
+          s.push(i)
+        }
+      })
+      return s;
+    default:
+     return state
+  }
 }
